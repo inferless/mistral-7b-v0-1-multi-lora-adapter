e956e2d14baa18b41c30cf9f0f3e1d524bbcf71e
Chintan <chintanckg@gmail.com>
2024-08-24 11:30:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,22 +1,22 @@
 from transformers import AutoModelForCausalLM,AutoTokenizer
 from peft import PeftModel
+import os
 
+HF_TOKEN = os.environ.get("HF_TOKEN")
 
 class InferlessPythonModel:
     def initialize(self):
-        self.tokenizer = AutoTokenizer.from_pretrained("mistralai/Mistral-7B-v0.1")
-        base_model = AutoModelForCausalLM.from_pretrained("mistralai/Mistral-7B-v0.1").to("cuda")
-        self.model = PeftModel.from_pretrained(base_model,"CATIE-AQ/mistral7B-FR-InstructNLP-LoRA", adapter_name="french")
-        self.model.load_adapter("Liu-Xiang/mistral7bit-lora-sql", adapter_name="sql")
-        self.model.load_adapter("alignment-handbook/zephyr-7b-dpo-lora", adapter_name="dpo")
-        self.model.load_adapter("uukuguy/Mistral-7B-OpenOrca-lora", adapter_name="orca")
+        self.tokenizer = AutoTokenizer.from_pretrained("unsloth/Meta-Llama-3.1-8B-Instruct")
+        base_model = AutoModelForCausalLM.from_pretrained("unsloth/Meta-Llama-3.1-8B-Instruct").to("cuda")
+        self.model = PeftModel.from_pretrained(base_model,"ushuradmin/usrs-extractions-adapteronly-16_bit_llama3.1_v1", adapter_name="extract", token = HF_TOKEN)
+        self.model.load_adapter("ushuradmin/usrs-classification-adapteronly-16_bit_llama3.1_v1", adapter_name="classify", token = HF_TOKEN)
 
     def infer(self, inputs):
         prompt = inputs["prompt"]
         adapter_name = inputs.pop("adapter_name")
         temperature = inputs.get("temperature",0.7)
         repetition_penalty = float(inputs.get("repetition_penalty",1.18))
-        max_new_tokens = inputs.get("max_new_tokens",128)
+        max_new_tokens = inputs.get("max_new_tokens",1024)
         
         if (self.model.active_adapter) != adapter_name:
             self.model.set_adapter(adapter_name)
